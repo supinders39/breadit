fd9a8481a7d1c05565c77ad3c3afe6ea5976da30
Supinder Singh <supinders39@gmail.com>
2023-11-19 05:24:19 +0530

    displaying posts in feed implemented

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import MiniCreatePost from '@/components/MiniCreatePost';
+import PostFeed from '@/components/PostFeed';
 import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/config';
 import { getAuthSession } from '@/lib/auth';
 import { db } from '@/lib/db';
@@ -28,17 +29,18 @@ const page = async ({ params }: pageProps) => {
             }
         }
     })
-    
+
     if (!subreddit) {
         return notFound()
     }
 
     return <>
         <h1 className=' font-bold text-3xl md:text-4xl h-1/4'>
-            r/{subreddit.name }
+            r/{subreddit.name}
         </h1>
         <MiniCreatePost session={session} />
         {/* TODO: Show posts in user feed  */}
+        <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} />
     </>
 }
 
