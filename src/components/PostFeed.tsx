84f054ee365febf169dc1dd16fd0cffcbf42717a
Supinder Singh <supinders39@gmail.com>
2023-11-19 11:16:43 +0530

    voting for posts implemented client side

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,12 +1,15 @@
-"use client"
+'use client'
+
 import { ExtendedPost } from '@/types/db'
-import { FC, useRef } from 'react'
 import { useIntersection } from '@mantine/hooks'
 import { useInfiniteQuery } from '@tanstack/react-query'
-import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/config'
 import axios from 'axios'
-import { useSession } from 'next-auth/react'
+import { Loader2 } from 'lucide-react'
+import { FC, useEffect, useRef } from 'react'
 import Post from './Post'
+import { useSession } from 'next-auth/react'
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/config'
+
 interface PostFeedProps {
   initialPosts: ExtendedPost[]
   subredditName?: string
@@ -16,47 +19,84 @@ const PostFeed: FC<PostFeedProps> = ({ initialPosts, subredditName }) => {
   const lastPostRef = useRef<HTMLElement>(null)
   const { ref, entry } = useIntersection({
     root: lastPostRef.current,
-    threshold: 1
+    threshold: 1,
   })
-
   const { data: session } = useSession()
 
-  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(['infinite-query'], async ({ pageParam = 1 }) => {
-    const query = `/api/posts?limit=${INFINITE_SCROLLING_PAGINATION_RESULTS}&page=${pageParam}` +
-      (!!subredditName ? `&subredditName=${subredditName}` : '');
+  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
+    ['infinite-query'],
+    async ({ pageParam = 1 }) => {
+      const query =
+        `/api/posts?limit=${INFINITE_SCROLLING_PAGINATION_RESULTS}&page=${pageParam}` +
+        (!!subredditName ? `&subredditName=${subredditName}` : '')
 
-    const { data } = await axios.get(query);
-
-    return data as ExtendedPost[]
-  }, {
-    getNextPageParam: (_, pages) => {
-      return pages.length + 1;
+      const { data } = await axios.get(query)
+      return data as ExtendedPost[]
     },
-    initialData: { pages: [initialPosts], pageParams: [1] }
-  })
 
-  const posts = data?.pages.flatMap((page) => page) ?? initialPosts;
+    {
+      getNextPageParam: (_, pages) => {
+        return pages.length + 1
+      },
+      initialData: { pages: [initialPosts], pageParams: [1] },
+    }
+  )
 
-  return <ul className='flex flex-col col-span-2 space-y-6'>
-    {posts.map((post, index) => {
-      const votesAmt = post.votes.reduce((acc, vote) => {
-        if (vote.type === "UP") return acc + 1;
-        if (vote.type === "DOWN") return acc - 1
-        return acc
-      }, 0)
-      // @ts-ignore
-      const currentVote = post.votes.find((vote) => vote.userId === session?.user?.id)
-      if (index === posts.length - 1) {
-        return (
-          <li key={post.id} ref={ref}>
-            <Post commentAmt={post.comments.length} post={post} subredditName={post.subreddit.name} />
-          </li>
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage() // Load more posts when the last post comes into view
+    }
+  }, [entry, fetchNextPage])
+
+  const posts = data?.pages.flatMap((page) => page) ?? initialPosts
+
+  return (
+    <ul className='flex flex-col col-span-2 space-y-6'>
+      {posts.map((post, index) => {
+        const votesAmt = post.votes.reduce((acc, vote) => {
+          if (vote.type === 'UP') return acc + 1
+          if (vote.type === 'DOWN') return acc - 1
+          return acc
+        }, 0)
+
+        const currentVote = post.votes.find(
+          (vote) => vote.userId === session?.user?.id
         )
-      } else {
-        return <Post commentAmt={post.comments.length} post={post} subredditName={post.subreddit.name} />
-      }
-    })}
-  </ul>
+
+        if (index === posts.length - 1) {
+          // Add a ref to the last post in the list
+          return (
+            <li key={post.id} ref={ref}>
+              <Post
+                post={post}
+                commentAmt={post.comments.length}
+                subredditName={post.subreddit.name}
+                votesAmt={votesAmt}
+                currentVote={currentVote}
+              />
+            </li>
+          )
+        } else {
+          return (
+            <Post
+              key={post.id}
+              post={post}
+              commentAmt={post.comments.length}
+              subredditName={post.subreddit.name}
+              votesAmt={votesAmt}
+              currentVote={currentVote}
+            />
+          )
+        }
+      })}
+
+      {isFetchingNextPage && (
+        <li className='flex justify-center'>
+          <Loader2 className='w-6 h-6 text-zinc-500 animate-spin' />
+        </li>
+      )}
+    </ul>
+  )
 }
 
 export default PostFeed
