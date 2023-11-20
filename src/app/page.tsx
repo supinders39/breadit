bc1b13045a6071857a416bd39c92be73e76f0c30
Supinder Singh <supinders39@gmail.com>
2023-11-20 07:04:02 +0530

    feed optimization implemented with redis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+import GeneralFeed from "@/components/GeneralFeed";
 import { buttonVariants } from "@/components/ui/Button";
+import { getAuthSession } from "@/lib/auth";
 import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession();
   return <>
   <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
     <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-      {/* feed */}
+      {/* disply either custom feed or general feed */}
+      {/* @ts-expect-error server component  */}
+      {session ? <CustomFeed /> : <GeneralFeed />}
 
        
       {/* subredit info */}
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,5 +1,5 @@
 import { Redis } from '@upstash/redis';
 export const redis = new Redis({
     url: process.env.REDIS_URL!,
-    token: process.env.REDIS_TOKEN!
+    token: process.env.REDIS_SECRET!
 })
