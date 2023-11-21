1367ee718ab9d8059d7f76e4ec5dc74e4fd52b6d
Supinder Singh <supinders39@gmail.com>
2023-11-21 08:40:06 +0530

    ui fix

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -64,10 +64,10 @@ const Post: FC<PostProps> = ({
                         className='relative text-sm max-h-40 w-full overflow-clip'
                         ref={pRef}>
                         <EditorOutput content={post.content} />
-                        {pRef.current?.clientHeight === 160 ? (
+                        {/* {pRef.current?.clientHeight === 160 ? (
                             // blur bottom if content is too long
                             <div className='absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent'></div>
-                        ) : null}
+                        ) : null} */}
                     </div>
                 </div>
             </div>
